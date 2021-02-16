a5a32feb488f99e842ce7f03e99e44c1b2b8d624
Samuel Souza <samuellrrs@gmail.com>
2021-02-16 13:12:56 -0300

    feat: 'Adicionado navegação por tabs'

---
+++ script.js
@@ -0,0 +1,33 @@
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+  if (tabContent.length && tabMenu.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+
+const accordionList = document.querySelectorAll(".js-accordion dt");
+
+function activeAccordion() {
+  this.classList.add("ativo");
+  this.nextElementSibling.classList.add("ativo");
+}
+
+accordionList.forEach((item) => {
+  item.addEventListener("click", activeAccordion);
+});
