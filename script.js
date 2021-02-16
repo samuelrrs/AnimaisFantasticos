7fdf6f9dfc9b07324d470e5e5fbdfaa07b8462b1
Samuel Souza <samuellrrs@gmail.com>
2021-02-16 14:03:23 -0300

    feat: Adicionado accordion nas informações do faq

--- script.js
+++ script.js
@@ -21,13 +21,21 @@ function initTabNav() {
 }
 initTabNav();
 
-const accordionList = document.querySelectorAll(".js-accordion dt");
+function accordionInit() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
 
-function activeAccordion() {
-  this.classList.add("ativo");
-  this.nextElementSibling.classList.add("ativo");
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass)
+    accordionList[0].nextElementSibling.classList.add(activeClass)
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
 }
-
-accordionList.forEach((item) => {
-  item.addEventListener("click", activeAccordion);
-});
+accordionInit()
