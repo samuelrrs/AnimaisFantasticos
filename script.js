32c5ab01c7bb85fd3aaffe4cba82f19af65fd6fd
Samuel Souza <samuellrrs@gmail.com>
2021-03-04 14:08:43 -0300

    feat: adição do index principal e do js

--- script.js
+++ script.js
@@ -1,84 +1,63 @@
-function initTabNav() {
-  const tabMenu = document.querySelectorAll('.js-tabmenu li');
-  const tabContent = document.querySelectorAll('.js-tabcontent section');
+const controles = document.getElementById('controles');
+const cssText = document.querySelector('.css');
+const btn = document.querySelector('.btn');
+controles.addEventListener('change', handleChange);
 
-  if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
-
-    function activeTab(index) {
-      tabContent.forEach((section) => {
-        section.classList.remove('ativo');
-      });
-      tabContent[index].classList.add('ativo');
-    }
-
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
-    });
-  }
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + 'px';
+  },
+  width(value) {
+    this.element.style.width = value + 'px';
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + 'px';
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + 'rem';
+  },
 }
-initTabNav();
 
-function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
-  const activeClass = 'ativo';
-  
-  if(accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
 
-    function activeAccordion() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
-
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
-    });
-  }
+  handleStyle[name](value);
+  saveValues(name, value);
+  showCss();
 }
-initAccordion();
 
-function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
 
-  function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-  }
-
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
+function setValues() {
+  const properties = Object.keys(localStorage);
+  properties.forEach(propertie => {
+    handleStyle[propertie](localStorage[propertie]);
+    controles.elements[propertie].value = localStorage[propertie];
   });
+  showCss();
 }
-initScrollSuave();
 
-function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
-  if(sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
+setValues();
 
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
-          section.classList.add('ativo');
-        else 
-          section.classList.remove('ativo');
-      })
-    }
-
-    animaScroll();
-
-    window.addEventListener('scroll', animaScroll);
-  }
+function showCss() {
+  cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
 }
-initAnimacaoScroll();
